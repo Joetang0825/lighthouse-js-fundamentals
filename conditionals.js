154804fbad8928ef1bdbdb58ff9bec8fd0dc3abc
Joe Tang <joetang81@gmail.com>
2021-12-14 17:19:35 -0500

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,14 @@
+const raining = true;
+const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
 const temperature = 12;
 
 if (temperature < 0) {
@@ -9,3 +20,15 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
